8625d2a92bc4a0c1858a6313e490c05af0c0311e
Codify01-MacAir <codify01-macair@Codify01-MacAirs-MacBook-Air.local>
2019-10-08 22:19:46 +0700

    add fraudforce

--- android/src/main/java/id/codify/sapio/SapioPlugin.java
+++ android/src/main/java/id/codify/sapio/SapioPlugin.java
@@ -1,24 +1,44 @@
 package id.codify.sapio;
 
+import android.content.Context;
+
 import com.getcapacitor.JSObject;
 import com.getcapacitor.NativePlugin;
 import com.getcapacitor.Plugin;
 import com.getcapacitor.PluginCall;
 import com.getcapacitor.PluginMethod;
+import credoapp.CredoAppService;
+import credoapp.CredoAppException;
+import credoapp.ErrorType;
 
 @NativePlugin()
 public class SapioPlugin extends Plugin {
 
     @PluginMethod()
     public void init(PluginCall call) {
-        String authKey = call.getString("authKey");
-        String url = call.getString("url");
-        String recordNumber = call.getString("recordNumber");
+        try {
+            Context context = getContext();
+            String authKey = call.getString("authKey");
+            String url = call.getString("url");
+            String recordNumber = call.getString("recordNumber");
 
-        JSObject ret = new JSObject();
-        ret.put("authKey", authKey);
-        ret.put("url", url);
-        ret.put("recordNumber", recordNumber);
-        call.success(ret);
+            CredoAppService credoAppService = new CredoAppService(context, url, authKey);
+
+            credoAppService.collectData(recordNumber);
+
+            JSObject ret = new JSObject();
+            ret.put("message", "Success record " + recordNumber);
+            call.success(ret);
+        }
+        catch (CredoAppException e){
+            ErrorType errorType = e.getType();
+            String message = e.getMessage();
+
+            JSObject err = new JSObject();
+            err.put("errorType", errorType);
+            err.put("errorMessage", message);
+            call.error(err.toString());
+        }
+
     }
 }
