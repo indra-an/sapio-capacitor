fb9107c2f51362c812121664d160e329dae03c95
indra <indra.abdullah99@gmail.com>
2019-10-16 15:29:39 +0700

    update

--- android/src/main/java/id/codify/sapio/SapioPlugin.java
+++ android/src/main/java/id/codify/sapio/SapioPlugin.java
@@ -2,7 +2,10 @@ package id.codify.sapio;
 
 import android.Manifest;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.os.Build;
+import android.provider.Settings;
 import android.util.Log;
 
 import com.getcapacitor.JSObject;
@@ -12,22 +15,25 @@ import com.getcapacitor.PluginCall;
 import com.getcapacitor.PluginMethod;
 import com.getcapacitor.PluginRequestCodes;
 
+import java.util.Collection;
+
 import credoapp.CredoAppService;
 import credoapp.CredoAppException;
 import credoapp.ErrorType;
 
+
 @NativePlugin(
-//    permissions={
-//        Manifest.permission.INTERNET,
-//        Manifest.permission.READ_EXTERNAL_STORAGE,
-//        Manifest.permission.READ_CONTACTS,
-//        Manifest.permission.READ_CALENDAR,
-//        Manifest.permission.GET_ACCOUNTS,
-//        Manifest.permission.READ_PHONE_STATE,
-//        Manifest.permission.BLUETOOTH,
-//        Manifest.permission.ACCESS_WIFI_STATE,
-//        Manifest.permission.PACKAGE_USAGE_STATS
-//    }
+    permissions={
+        Manifest.permission.INTERNET,
+        Manifest.permission.READ_EXTERNAL_STORAGE,
+        Manifest.permission.READ_CONTACTS,
+        Manifest.permission.READ_CALENDAR,
+        Manifest.permission.GET_ACCOUNTS,
+        Manifest.permission.READ_PHONE_STATE,
+        Manifest.permission.BLUETOOTH,
+        Manifest.permission.ACCESS_WIFI_STATE,
+        Manifest.permission.PACKAGE_USAGE_STATS,
+    }
 )
 
 public class SapioPlugin extends Plugin {
@@ -36,48 +42,59 @@ public class SapioPlugin extends Plugin {
     @PluginMethod()
 
     public void init(PluginCall call) {
-        if (!hasRequiredPermissions()) {
-            saveCall(call);
+
+//        if (!hasRequiredPermissions()) {
+//            saveCall(call);
 //            pluginRequestAllPermissions();
+//
+//            pluginRequestPermissions(new String[] {
+//                    Manifest.permission.INTERNET,
+//                    Manifest.permission.READ_EXTERNAL_STORAGE,
+//                    Manifest.permission.READ_CONTACTS,
+//                    Manifest.permission.READ_CALENDAR,
+//                    Manifest.permission.GET_ACCOUNTS,
+//                    Manifest.permission.READ_PHONE_STATE,
+//                    Manifest.permission.BLUETOOTH,
+//                    Manifest.permission.ACCESS_WIFI_STATE,
+//                    Manifest.permission.PACKAGE_USAGE_STATS
+//            }, REQUEST_IMAGE_CAPTURE);
+//        } else {
+        Context context = getContext();
+        String authKey = call.getString("authKey");
+        String url = call.getString("url");
+        String recordNumber = call.getString("recordNumber");
 
-            pluginRequestPermissions(new String[] {
-                    Manifest.permission.INTERNET,
-                    Manifest.permission.READ_EXTERNAL_STORAGE,
-                    Manifest.permission.READ_CONTACTS,
-                    Manifest.permission.READ_CALENDAR,
-                    Manifest.permission.GET_ACCOUNTS,
-                    Manifest.permission.READ_PHONE_STATE,
-                    Manifest.permission.BLUETOOTH,
-                    Manifest.permission.ACCESS_WIFI_STATE,
-                    Manifest.permission.PACKAGE_USAGE_STATS
-            }, REQUEST_IMAGE_CAPTURE);
-        } else {
-            try {
-                Context context = getContext();
-                String authKey = call.getString("authKey");
-                String url = call.getString("url");
-                String recordNumber = call.getString("recordNumber");
+        try {
 
-                CredoAppService credoAppService = new CredoAppService(context, url, authKey);
+            CredoAppService credoAppService = new CredoAppService(context, url, authKey);
+            credoAppService.setIgnorePermission(false);
+            final Collection<String> ungrantedPermissions = credoAppService.getUngrantedPermissions();
 
-                String res = credoAppService.collectData(recordNumber);
-
-                JSObject ret = new JSObject();
-                ret.put("result", res);
-                ret.put("message", "Success record " + recordNumber);
-                call.success(ret);
-
+            // Check data usage statistics permission. In order to access Application and Network usage statistics, the user should manually grant 'Usage Stats' permission
+            if (ungrantedPermissions.contains("android.permission.PACKAGE_USAGE_STATS")) {
+                startActivityForResult(getSavedCall(), new Intent(Settings.ACTION_USAGE_ACCESS_SETTINGS), 0);
+            } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && !ungrantedPermissions.isEmpty()) {
+                pluginRequestPermissions(ungrantedPermissions.toArray(new String[ungrantedPermissions.size()]), REQUEST_IMAGE_CAPTURE);
             }
-            catch (CredoAppException e){
-                ErrorType errorType = e.getType();
-                String message = e.getMessage();
 
-                JSObject err = new JSObject();
-                err.put("errorType", errorType);
-                err.put("errorMessage", message);
-                call.error(err.toString());
-            }
+            final String res = credoAppService.collectData(recordNumber);
+
+            JSObject ret = new JSObject();
+            ret.put("result", res);
+            ret.put("message", "Success record " + recordNumber);
+            call.success(ret);
+
         }
+        catch (CredoAppException e){
+            ErrorType errorType = e.getType();
+            String message = e.getMessage();
+
+            JSObject err = new JSObject();
+            err.put("errorType", errorType);
+            err.put("errorMessage", message);
+            call.error(err.toString());
+        }
+//        }
     }
 
     @Override
